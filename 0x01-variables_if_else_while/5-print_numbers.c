ef2b6937de143c3fec9a9298fc7ed58e37559448
Jay stones <Joelpaul345@gmail.com>
2022-09-09 22:06:52 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,11 +10,10 @@ int main(void)
         int n = 0;
 
         while (n <= 9)
-        {
+        
                    printf("%d", n);
                    n++;
-        }
-        printf("\n");
+                   printf("\n");
         
         return (0);
 }
