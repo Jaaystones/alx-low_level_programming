6e79e76a18c45dca2056664550ef82f82d4fb61e
Jay stones <Joelpaul345@gmail.com>
2022-09-10 07:51:50 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - prints all possible combinations of two two-digit numbers.
- *
- * Return: Always 0 (Success)
- */
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-	      int d;
+	int c, i, k, j;
 
-	      for (d = 0; d < 90; d++)
-	      {
-		            for ()
-		            {
-                        putchar((d / 10) + '0');
-			                  putchar((d % 10) + '0');
-		            }
-		            if (d != 89)
-		            {
-			                  putchar(',');
-			                  putchar(' ');
-		            }
-	      }
-	      putchar('\n');
-	      return (0);
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
+
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
