676378deee5ce8e2b5b34a9d8b6dc6ca6c0312d5
Jay stones <Joelpaul345@gmail.com>
2022-09-13 16:44:40 +0100

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,12 +2,23 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * main - Entry point
+ * main -  prints the sum of all the multiples of 3 or 5 below 1024
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{
+{ 
+	int sum = 0;
 
-	return (0);
+	for (int i = 3; i < 1024; i++) 
+	{
+
+		if (i%3==0 or i%5==0)
+
+			sum += i;
+	}
+
+	cout << sum;
+
+	return 0;
 }
