be6c272c08f7571e684b1168d328e3ac4699b509
Jay stones <Joelpaul345@gmail.com>
2022-09-13 13:04:37 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - Write a program that prints _putchar, followed by a new line.
+ * main - Write a program that prints putchar, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
