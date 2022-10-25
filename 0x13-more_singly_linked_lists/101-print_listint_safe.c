406ec0d9cab01757c6c06c2f11e8fe070560686e
Jay stones <Joelpaul345@gmail.com>
2022-10-25 06:41:53 +0100

    Advanced tasks

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,22 +16,17 @@ size_t print_listint_safe(const listint_t *head)
 	tmp = head;
 	if (tmp == 0)
 		return (0);
-
 	while (tmp != 0)
 	{
 		tmp2 = tmp;
 		tmp = tmp->next;
-
 		count++;
-
 		printf("[%p] %d\n", (void *)tmp2, tmp2->n);
-
 		if (tmp2 <= tmp)
 		{
 			printf("-> [%p] %d\n", (void *)tmp, tmp->n);
 			break;
 		}
 	}
-
 	return (count);
 }
