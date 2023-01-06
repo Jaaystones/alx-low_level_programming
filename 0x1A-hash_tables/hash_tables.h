fd5f8f03c355f0d4f15671ca45dfa88c700bf6be
Jaaystones <Joelpaul345@gmail.com>
2023-01-05 22:26:24 -0800

    Hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,10 +40,6 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 1cd10f6fb6f659afab90a58eb438c066de5087ba
 /**
  * struct shash_node_s - Node of a sorted hash table
  * @key: The key, string
@@ -86,8 +82,4 @@ void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-<<<<<<< HEAD
-#endif /* HASH_TABLES_HEADER */
-=======
 #endif /* HASH_TABLES_H */
->>>>>>> 1cd10f6fb6f659afab90a58eb438c066de5087ba
